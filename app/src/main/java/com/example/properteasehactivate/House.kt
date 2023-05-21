fedfa04452f5565af01e5b0f34c8afa14457d759
ajeeshrawal <ajeeshrawal3376@gmail.com>
2023-05-21 12:00:27 +0530

    task

---
+++ app/src/main/java/com/example/properteasehactivate/House.kt
@@ -0,0 +1,6 @@
+package com.example.properteasehactivate
+
+
+data class House(val photo: Int, val price: String, val bhk: String, val sqft: String)
+
+
---
+++ app/src/main/java/com/example/properteasehactivate/ownerModel.kt
@@ -0,0 +1,12 @@
+package com.example.properteasehactivate
+
+data class OwnerModel(
+    var ownerid: String? = null,
+    var name: String? = null,
+    var phoneno: String? = null,
+    var address: String? = null,
+    var area: String? = null,
+    var price: String? = null,
+    var bhk: String? = null,
+    var park: String?=null
+)
